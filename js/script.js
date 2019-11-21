d3421c40725b51ff7fe6045c3ea6fd5287f93578
Shelley Dogra <shelleycdogra@lewisu.edu>
2019-11-21 10:32:29 -0600

    disable local js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-(function(){
-'use strict';
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var hd = document.getElementById('heading-text');
-
-
-        setInterval( function updateOne() {
-            var one = "Patrick Tang";
-           
-            
-            hd.innerHTML = one;
-
-
-                    } , 0);
-                    
-                    
-        setInterval( function updateTwo() {
-            
-            var two = "Bankruptcy";
-            
-            
-            hd.innerHTML = two;
-
-
-                    } , 3000);
-                    
-                    
-        setInterval( function updateThree() {
-            
-            var three = "Real Estate";
-            
-            hd.innerHTML = three;
-
-
-                    } , 6000);
-
-        
-
-});
-
-})();
+// (function(){
+// 'use strict';
+//
+//     document.addEventListener('DOMContentLoaded', function() {
+//         var hd = document.getElementById('heading-text');
+//
+//
+//         setInterval( function updateOne() {
+//             var one = "Patrick Tang";
+//
+//
+//             hd.innerHTML = one;
+//
+//
+//                     } , 0);
+//
+//
+//         setInterval( function updateTwo() {
+//
+//             var two = "Bankruptcy";
+//
+//
+//             hd.innerHTML = two;
+//
+//
+//                     } , 3000);
+//
+//
+//         setInterval( function updateThree() {
+//
+//             var three = "Real Estate";
+//
+//             hd.innerHTML = three;
+//
+//
+//                     } , 6000);
+//
+//
+//
+// });
+//
+// })();
